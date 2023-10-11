03245d3018618c0bc9d324cc87c83bf26cebd67c
poiu <75510171+notpoiu@users.noreply.github.com>
2023-10-11 13:54:36 -0400

    Alternative HTTP request

---
+++ http/HttpResponse.java
@@ -0,0 +1,19 @@
+package dev.upio.tidalwave.utils.http;
+
+public class HttpResponse {
+    int statusCode;
+    String body;
+
+    public HttpResponse(int statusCode, String body) {
+        this.statusCode = statusCode;
+        this.body = body;
+    }
+
+    public int statusCode() {
+        return statusCode;
+    }
+
+    public String body() {
+        return body;
+    }
+}
